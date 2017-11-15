7bd5a1b541a299cfa28cd9978b666641a8b5535d
sunpeng <sunpeng@buestc.com>
2017-11-15 11:25:00 +0800

    项目开始

---
+++ tallyBook/tallyBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tallyBook
+//
+//  Created by sunpeng on 2017/11/15.
+//  Copyright © 2017年 mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tallyBook/tallyBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tallyBook
+//
+//  Created by sunpeng on 2017/11/15.
+//  Copyright © 2017年 mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
